c542ae024d11ccebb5654ab615c9ae4b3d2038b3
tclaughridge <tcl5tu@virginia.edu>
2022-10-19 16:05:56 -0400

    Created test maker

---
+++ Trading/Testmaker.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <random>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    string filename = argv[1];
+    int numpts = stoi(argv[2]);
+
+    int LO = -50000;
+    int HI = 50000;
+
+    ofstream file(filename);
+
+    // Write to the file
+    file << numpts << endl;
+    for (int i = 0; i < numpts; i++) {
+        float r1 = LO + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(HI-LO)));
+        float r2 = LO + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(HI-LO)));
+        file << r1 << " " << r2 << endl;
+    }
+
+    file.close();
+
+    return 0;
+}
